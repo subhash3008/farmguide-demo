5b19a1ba6c90f476f391b24f9ca10311224a41e0
Subhash Chandra <subhash@9stacks.com>
2019-12-14 17:12:34 +0530

    minor changes

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import * as utils from '../components/utils';
 
 export const _api = axios.create({
-    baseURL: 'http://farmguide-demo.firebaseio.com/',
+    baseURL: 'https://farmguide-demo.firebaseio.com/',
 });
 
 _api.interceptors.response.use(
